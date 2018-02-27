59962220e30d89857707b408e64717e7a3e6c017
anudroid91 <anurag.n@darwinbox.io>
2018-02-27 13:44:12 +0530

    done

--- app/src/main/java/com/darwinbox/demo/BaseActivity.java
+++ app/src/main/java/com/darwinbox/demo/BaseActivity.java
@@ -1,17 +1,28 @@
 package com.darwinbox.demo;
 
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
     }
 
     public abstract void fabClicked(View view);
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+
+        RelativeLayout relativeLayout = (RelativeLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
+        FrameLayout frameLayout = relativeLayout.findViewById(R.id.frame);
+        getLayoutInflater().inflate(layoutResID, frameLayout, true);
+        super.setContentView(relativeLayout);
+    }
 }
